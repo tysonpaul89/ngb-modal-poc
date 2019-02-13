b13eea8f759df21753834fad496bdfcd34345ccd
Tyson Paul <tyson.paul@digitalmesh.com>
2019-02-13 12:20:14 +0530

    src path removed

--- src/app/test/test/test.component.ts
+++ src/app/test/test/test.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageComponent } from 'src/app/message/message/message.component';
+import { MessageComponent } from '../../message/message/message.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
